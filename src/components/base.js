072d08041299f5bd8f2372a20ea0f9758b3e194d
aditya subramanyam <subramanyam963@gmail.com>
2018-02-09 00:53:24 +0530

    initial setup done

---
+++ src/components/base.js
@@ -0,0 +1,8 @@
+/** @jsx createElement */
+import createElement from '../utils/create-element'
+
+export default class Component {
+  render() {
+    return <div></div>;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Component from './components/base'
+import createElement from './utils/create-element'
+export default {
+    Component,
+    createElement
+};
